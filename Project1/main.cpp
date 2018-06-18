eab9a58ed52445c777a3a6cab23f2f8eb8620200
谢江钊 <10751583@qq.com>
2018-06-18 13:17:43 +0800

    Good afternoon

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -14,6 +14,7 @@ int own_N[9126] = { 0 };
 short C[9126][9126];
 short own_C[9126][9126];
 float W[9126][9126];
+float own_W[9126][9126];
 map<string, int>  idmap;
 float get_score(vector<pair<int, float>>& src, int id) {
 	for (int i = 0; i < src.size(); i++)
@@ -47,35 +48,41 @@ vector<pair<int, float>>* itembase(vector<vector<string>> src, vector<pair<int, 
 	}
 	return res;
 }
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {	
 	int myid, numprocs, source;
 	int namelen;
 	char processor_name[MPI_MAX_PROCESSOR_NAME];
 	MPI_Status status;
 	MPI_Init(&argc, &argv); //初始化MPI环境
-	int xjz[2][2] = { { 1,2 },{ 3,4 } };
-	int asn[2][2] = { { 1,2 },{ 3,4 } };
+	long beg = clock();
 	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
 	//MPI_Bcast(&xjz, 4, MPI_INT,0, MPI_COMM_WORLD);
 	MPI_Get_processor_name(processor_name, &namelen);
-	cout << "Hello Wor!Process " << myid << " of %d on Hello World!Process " << numprocs << " of " << processor_name << "ddd" << xjz[0][0] << endl;
-	asn[0][1] = 7;
-	cout << "asn" << asn[0][1] << endl;
+	cout << "Hello Wor!Process " << myid << " of %d on Hello World!Process " << numprocs << " of " << processor_name << endl;
 	vector<pair<int, float>> res[672];
 	vector<pair<int, float>> test[672];
 	memset(C, 0, sizeof(short) * 9126 * 9126);
 	memset(own_C, 0, sizeof(short) * 9126 * 9126);
+	cout << "Process " << myid << " startcsvtovector" << endl;
+	beg = clock();
 	Context= csvtovector("movies.csv");
 	Buildmap(Context);
 	Context=csvtovector("ratings.csv");
+	cout << "Process " << myid << " use " << clock()-beg <<endl;
+	cout << "Process " << myid << " start spilt set" << endl;
+	beg = clock();
 	vector<vector <string> > train_set = get_train_set(Context);
 	vector<vector <string> > test_set = get_test_set(Context);
+	cout << "Process " << myid << " use " << clock() - beg << endl;
+	cout << "Process " << myid << " start itembase " << endl;
+	beg = clock();
 	itembase(train_set,res);
 	itembase(test_set, test);
-	long beg = clock_t();
-	cout << "start doing" << endl;
-	for (int i = 672*myid/4; i < 672; i++)
+	cout << "Process " << myid << " use " << clock() - beg << endl;
+	cout << "Process " << myid << " start count" << endl;
+	beg = clock();
+	for (int i = 672*myid/4; i < 672 * (myid+1) / 4; i++)
 	{
 		for (int j = 0; j < res[i].size(); j++)
 		{
@@ -87,19 +94,23 @@ int main(int argc, char *argv[]) {
 			}
 		}
 	}
-	cout << "Process " << myid << "Finish in " << clock_t() - beg << endl;
-	MPI_Reduce(&own_N, &N, 9126, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-	MPI_Reduce(&own_C, &C, 9126*9126, MPI_SHORT, MPI_SUM, 0, MPI_COMM_WORLD);
-	MPI_Finalize(); //结束MPI环境
-	for (int i = 0; i < 9126; i++)
+	cout << "Process " << myid << "use " << clock() - beg << endl;
+	MPI_Allreduce(&own_N, &N, 9126, MPI_INT, MPI_SUM,  MPI_COMM_WORLD);
+	MPI_Allreduce(&own_C, &C, 9126*9126, MPI_SHORT, MPI_SUM,MPI_COMM_WORLD);
+	for (int i = 9126 * myid / 4; i < 9126 * (myid + 1) / 4; i++)
 	{
 		for (int j = 0; j < 9126; j++)
 		{
-			W[i][j] = C[i][j] / sqrt(N[i]*N[j]);
+			own_W[i][j] = C[i][j] / sqrt(N[i]*N[j]);
 		}
 	}
-	if (myid == 0) {
-		for (int i = 0; i < 672; i++)
+	MPI_Reduce(&own_W, &W, 9126*9126, MPI_INT, MPI_SUM, 0,MPI_COMM_WORLD);
+	if (myid ==0) {
+		for (int i = 0; i < 10; i++)
+		{
+			cout << N[i] << endl;
+		}
+		for (int i = 0; i < 10; i++)
 		{
 			cout << "User" << i << endl;
 			for (int j = 0; j < test[i].size(); j++)
@@ -114,5 +125,6 @@ int main(int argc, char *argv[]) {
 			}
 		}
 	}
+	MPI_Finalize(); //结束MPI环境
 	return 0;
 }
